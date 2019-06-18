51a7feef48130cecdccd45baf79c5c9167dad577
DELL\Manu <manugbhat@gmail.com>
2019-06-18 05:36:35 +0530

    Fix correct EP

--- src/app/constants/api-constants.ts
+++ src/app/constants/api-constants.ts
@@ -1,5 +1,5 @@
 export class APIConstants {
-    public static readonly API_ENDPOINT: string = "http://localhost:3000/";
+    public static readonly API_ENDPOINT: string = "http://139.59.59.84/";
 
     public static readonly HTTP_HEADERS: any = { "accept": "application/json", "Content-Type": "application/json" } ;
 }
